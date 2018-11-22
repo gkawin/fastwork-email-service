3763ae2af3332bd7a4943fe79806948a6c73e474
Kawin Glomjai <g.kawin@live.com>
2018-11-22 22:52:09 +0700

    mail builder

---
+++ packages/email-service/src/email/mail-builder.test.ts
@@ -0,0 +1,24 @@
+import MailBuilder from './MailBuilder'
+
+describe('MailBuilder', () => {
+  describe('when called stack', () => {
+    it('should be singleton and mixed the configuration object.', () => {
+      const buildConfig = new MailBuilder()
+      buildConfig
+        .to('example@testtst.com')
+        .subject("HELLO ITS ME")
+        .html('<div>WHATTTTTT</div>')
+        .retry(3)
+
+      const result = JSON.parse(buildConfig.export())
+
+      expect(result).toEqual(expect.objectContaining({
+        from: 'no_reply@thebanana.co',
+        html: '<div>WHATTTTTT</div>',
+        retry: 3,
+        subject: 'HELLO ITS ME',
+        to: 'example@testtst.com',
+      }))
+    })
+  })
+})
