68803cea84424800ddc4ee65f82f118e10f9213f
Kawin Glomjai <g.kawin@live.com>
2018-11-25 21:47:05 +0700

    adjust style

--- webapp/components/nav/LeftSidebar.js
+++ webapp/components/nav/LeftSidebar.js
@@ -1,7 +1,10 @@
 import './left-sidebar.styl'
+
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default class LeftSidebar extends React.PureComponent {
+  static propTypes = { children: PropTypes.any }
   render() {
     return (
       <section className='left-sidebar'>
